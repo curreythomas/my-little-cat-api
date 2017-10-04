34f858e71bf11258d2a054ac66e4d1453a51c50c
curreythomas <currey.thomas@gmail.com>
2017-10-04 12:03:05 -0400

    added propEq from Ramda

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const { filter, find } = require('ramda')
+const { filter, find, propEq } = require('ramda')
 
 const database = [
   { id: 'tabby', type: 'breed', description: 'A great breed.' },
